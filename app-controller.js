44ad6fa76b235121d315bb97f9e029bb7b66a3cc
Rishad Bharucha <rb2102@gmail.com>
2014-05-20 09:57:54 -0600

    Update controller with totaltodos

--- app-controller.js
+++ app-controller.js
@@ -1,6 +1,10 @@
 function TodoCtrl($scope) {
 	$scope.todos = [];
 
+	$scope.getTotalTodos = function() {
+    return $scope.todos.length;
+  };
+
 	$scope.addToDo = function() {
 		$scope.todos.push({text:$scope.toDoText, done:false});
 		$scope.toDoText = '';
@@ -10,8 +14,8 @@ function TodoCtrl($scope) {
 		$scope.todos.splice(todos.indexOf(todo), 1);
 	};
 
-	$scope.clearCompletedToDos = function () {
-		$scope.todos $scope.todos.filter(function (todo) {
+	$scope.clearCompletedToDos = function() {
+		$scope.todos $scope.todos.filter(function(todo) {
 			return !todo.completed;
 		});
 	};

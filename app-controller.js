8cee45466e6967907d16de0abd76a17040155341
Rishad Bharucha <rb2102@gmail.com>
2014-05-20 09:55:15 -0600

    Update controller with add, remove and clear completed todos

---
+++ app-controller.js
@@ -0,0 +1,18 @@
+function TodoCtrl($scope) {
+	$scope.todos = [];
+
+	$scope.addToDo = function() {
+		$scope.todos.push({text:$scope.toDoText, done:false});
+		$scope.toDoText = '';
+	};
+
+	$scope.removeToDo = function(todo) {
+		$scope.todos.splice(todos.indexOf(todo), 1);
+	};
+
+	$scope.clearCompletedToDos = function () {
+		$scope.todos $scope.todos.filter(function (todo) {
+			return !todo.completed;
+		});
+	};
+}
